17314d19d3453bd173713935072f38355e11994f
NicholasYerolemou <yerolemou.nicholas@gmail.com>
2022-03-01 08:58:25 +0200

    added comments and fixed bug taking user input

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
 from socket import*
 
-serverName = 'hostname'
-serverPort = 12000
+serverName = "127.0.0.1"  # set the servers IP address
+serverPort = 12000  # server port number
 
-clientSocket = socket(AF_INET,SOCK_DGRAM)
-message = raw_input("message")
-clientSocket.sendto(message.encode(),serverName,serverPort))
+# creates client socket. Paramater 1 indicates we are using IPv4. second parameter indicates the socket is of type written. i.e. a UDP socket.
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+message = input("input lower case sentence:")
+
+# message converted from string type to byte type.
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+# the message sent from the server  is put into the modified message
+# the packtes source address is put into serverAdress. it holds both the IP and port number.
+# 2048 is the buffer size
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-print modifiedMessage.decode()
+print(modifiedMessage.decode())  # bytes to string
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from socket import *
+
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind((", serverPort))
-    print ("The server is ready to recieve")
-    while True:
+serverSocket = socket(AF_INET, SOCK_DGRAM)  # creates socket
+# assigns the given port number to the servers socket
+serverSocket.bind(('', serverPort))
+
+print("The server is ready to recieve")
+
+while True:  # waits for a packet to arrive
+    # recives packet and client address (IP,port number)
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
+    modifiedMessage = message.decode().upper()  # puts the message in upper case
+    # sends it back to the client
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    # socket stays open and listening for more packets
