5bac77c24d3b9f7ae116f31675c65931508871b4
Collins Njane <NJNCOL001@myuct.ac.za>
2022-03-05 12:37:43 +0200

    Chat.py version 1.01(temporary)

--- Chat.py
+++ Chat.py
@@ -1,7 +1,19 @@
+from Message import *
 class Chat:
-    def __init__(self,chatHistory,chatId):
-        self.chatHistory = chatHistory
+    chatHistory = [[],[]]
+    ipAddress = ""
+    memberIps = []
+    msg = Message("","")
+    chatId = 0
+    def __init__(self,chatId):
         self.chatId = chatId
 
-    def getChatHistory(self,Id):
+
+    def getChatHistory(self,chatId):
         return self.chatHistory
+
+    def getChatId(self):
+        return self.chatId
+
+    #def addMessage(self, ipAddress , msg):
+
